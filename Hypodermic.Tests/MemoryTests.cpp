41be6561dd0763718e228d3d4d1fd86a817de144
Yohan <yohan.bainier@gmail.com>
2016-04-25 14:03:09 +0200

    MemoryTests introduced to validate some release behaviors

--- Hypodermic.Tests/MemoryTests.cpp
+++ Hypodermic.Tests/MemoryTests.cpp
@@ -4,17 +4,21 @@
 
 #include "TestingTypes.h"
 
+
 using namespace Hypodermic;
+
 
 BOOST_AUTO_TEST_SUITE(MemotyTests)
 
 BOOST_AUTO_TEST_CASE(container_should_be_freed)
 {
     // Arrange
-    std::weak_ptr<Container> weakContainer;
+    std::weak_ptr< Container > weakContainer;
     {
         ContainerBuilder builder;
+
         auto container = builder.build();
+
         weakContainer = container;
 
         // Act - exit from scope
@@ -28,13 +32,52 @@ BOOST_AUTO_TEST_CASE(container_should_be_freed)
 BOOST_AUTO_TEST_CASE(single_instance_should_be_freed)
 {
     // Arrange
-    std::weak_ptr<Testing::DefaultConstructible1> weakInstance;
+    std::weak_ptr< Testing::DefaultConstructible1 > weakInstance;
     {
-        ContainerBuilder builder;
-        builder.registerType<Testing::DefaultConstructible1>().singleInstance();
-        auto container = builder.build();
-        auto instance = container->resolve<Testing::DefaultConstructible1>();
-        weakInstance = instance;
+        std::shared_ptr< Container > container;
+        {
+            ContainerBuilder builder;
+            builder.registerType< Testing::DefaultConstructible1 >().singleInstance();
+
+            container = builder.build();
+        }
+
+        auto instance1 = container->resolve< Testing::DefaultConstructible1 >();
+        auto instance2 = container->resolve< Testing::DefaultConstructible1 >();
+
+        // Making sure this is not an on the fly registration
+        BOOST_REQUIRE(instance1 != nullptr);
+        BOOST_REQUIRE(instance1 == instance2);
+        weakInstance = instance1;
+
+        // Act - exit from scope
+    }
+
+    // Assert
+    auto instance = weakInstance.lock();
+    BOOST_CHECK(instance == nullptr);
+}
+
+BOOST_AUTO_TEST_CASE(provided_instance_should_be_freed)
+{
+    // Arrange
+    std::weak_ptr< Testing::DefaultConstructible1 > weakInstance;
+    {
+        std::shared_ptr< Container > container;
+        {
+            ContainerBuilder builder;
+            builder.registerInstance(std::make_shared< Testing::DefaultConstructible1 >());
+
+            container = builder.build();
+        }
+
+        auto instance1 = container->resolve< Testing::DefaultConstructible1 >();
+        auto instance2 = container->resolve< Testing::DefaultConstructible1 >();
+
+        // Making sure this is not an on the fly registration
+        BOOST_REQUIRE(instance1 != nullptr);
+        BOOST_REQUIRE(instance1 == instance2);
+        weakInstance = instance1;
 
         // Act - exit from scope
     }
